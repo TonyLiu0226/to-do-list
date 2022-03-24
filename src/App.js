f1cd6614678dcdc879483151b6aa1d975880bbc3
Tony Liu <tl0226yn@gmail.com>
2022-03-23 19:28:25 -0700

    New logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect} from 'react';
+import styled from 'styled-components'
+import { toHaveDescription } from '@testing-library/jest-dom/dist/matchers';
+
+const AppContainer = styled.div`
+ margin: 40vh 30vw;
+`
+function toDoItem(p) {
+  return (
+    <div>
+      <p>{p.name}</p>
+    </div>
+  )
+}
 
 function App() {
+  //initial entries in tdl
+  const [td, settd] = useState(["watch TV", "masturbate"])
   return (
-    <div className="App">
+    <AppContainer className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1>STRESSED CPEN STUDENT'S TODO LIST</h1>
+      </header> 
+      <h1>MY LIST</h1>
+      {td.map((item, i) => <toDoItem>
+        key={i}
+        name={item}
+      </toDoItem>)}
+    </AppContainer>
   );
 }
 
